868aa1cae99da21aab1d9cf104eea8e6fd10e02e
mr0crappy <kpratyush82@gmail.com>
2026-03-06 22:36:00 +0530

    Added some more vec2 functions

--- Include/Vec2.h
+++ Include/Vec2.h
@@ -2,6 +2,7 @@
 #define VEC2_H
 
 #include "Helpers.h"
+#include <cmath>
 
 namespace Math {
 struct Vec2 {
@@ -39,9 +40,41 @@ struct Vec2 {
     y /= a;
     return *this;
   }
-  inline constexpr float LengthSq() const {
-    return x * x + y * y;
-}
+  inline constexpr float LengthSq() const { return x * x + y * y; }
+  inline float Length() const { return std::sqrt(LengthSq()); }
+
+  static constexpr float Dot(const Vec2 &a, const Vec2 &b) {
+    return a.x * b.x + a.y * b.y;
+  }
+
+  inline Vec2 Normalized() const {
+    float len = Length();
+    if (IsZero(len)) {
+      return Vec2(0, 0);
+    }
+    return Vec2(x / len, y / len);
+  }
+
+  inline Vec2 &Normalize() {
+    float len = Length();
+    if (IsZero(len))
+      return *this;
+    x /= len;
+    y /= len;
+    return *this;
+  }
+
+  static constexpr float DistanceSq(const Vec2& a, const Vec2& b){
+    float dx = a.x-b.x;
+    float dy = a.y-b.y;
+    return dx*dx + dy*dy;
+  }
+
+  static float Distance(const Vec2& a, const Vec2& b){
+    return std::sqrt(DistanceSq(a,b));
+  }
+
+
 };
 } // namespace Math
 
